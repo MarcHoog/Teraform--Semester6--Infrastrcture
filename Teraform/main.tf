7502132c9708fe357c2f1dc690f65eb72403b491
Zamyza <marc.hoogendoorn@live.nl>
2021-10-13 20:29:34 +0200

    changed some hella cool things jo

--- Teraform/main.tf
+++ Teraform/main.tf
@@ -23,12 +23,10 @@ provider "proxmox" {
 } 
 
 resource "proxmox_lxc" "basic-test" {
-  count = 5
-  name = "test-vm-${count.index + 1}"
-
+  count = length(var.hostnames)
 
   target_node  = "ve-hp"
-  hostname     = "${name}"
+  hostname     = var.hostnames
   ostemplate   = "local:vztmpl/ubuntu-21.04-standard_21.04-1_amd64.tar.gz"
   password     = "BasicLXCContainer"
   unprivileged = true
---
+++ Teraform/var.tf
@@ -0,0 +1,4 @@
+variable "hostnames" {
+  type    = list
+  #default = ["terraformed-container"] 
+}
