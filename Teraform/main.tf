5162e244fe2c40fe57b2aad38e3b594f652cfca7
Zamyza <marc.hoogendoorn@live.nl>
2021-10-13 19:44:24 +0200

    main.tf changed

--- Teraform/main.tf
+++ Teraform/main.tf
@@ -6,3 +6,38 @@ terraform {
         }
     }
 }
+
+provider "proxmox" {
+
+    # full host name + /api2/json
+    pm_api_url = "https://192.168.1.252:8006/api2/json"
+
+    # API token id for user: teraform-test
+    pm_api_token_id ="teraform-test@pve!new_token_id"
+
+    # is just local so i don't care much atm but this isn't safe
+    pm_api_token_secret =  "a70d8aaa-d615-4eaa-acfb-88e59be062b4"
+
+    # NO SSL
+    pm_tls_insecure = true
+} 
+
+resource "proxmox_lxc" "basic-test" {
+  target_node  = "pve"
+  hostname     = "basic-test1"
+  ostemplate   = "local:vztmpl/ubuntu-20.04-standard_20.04-1_amd64.tar.gz"
+  password     = "BasicLXCContainer"
+  unprivileged = true
+
+  // Terraform will crash without rootfs defined
+  rootfs {
+    storage = "local-zfs"
+    size    = "8G"
+  }
+
+  network {
+    name   = "eth0"
+    bridge = "vmbr0"
+    ip     = "dhcp"
+  }
+}
