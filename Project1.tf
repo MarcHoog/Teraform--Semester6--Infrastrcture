f2e3b8fa3c7450d2d159f5038f6956deb90f169a
marc <marc.hoogendoorn@live.nl>
2022-04-06 13:05:57 +0200

    🎉 Started first terraform project

---
+++ Project1.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source  = "telmate/proxmox"
+      version = "2.9.6"
+    }
+  }
+}
+
+provider "proxmox" {
+  pm_api_url = "10.0.0.253:8006/api2/json"
+}
